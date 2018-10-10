391c9c745297d37385d94c250c254257c118fd83
yuzhongfengling <844784976@qq.com>
2018-10-10 14:11:54 +0800

    可滑动布局

---
+++ slidelayout/src/main/java/com/rainbell/www/slidelayout/ScrollerLayout.java
@@ -0,0 +1,121 @@
+package com.rainbell.www.slidelayout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+public class ScrollerLayout extends ViewGroup {
+
+    private Scroller mScroller;
+    private int time = 0;
+    /**
+     * 判定为拖动的最小移动像素数
+     */
+    private int mTouchSlop;
+    private float mYDown = 0;
+    private float mYMove = 0;
+
+
+    /**
+     * 上次触发ACTION_MOVE事件时的屏幕坐标
+     */
+    private float mYLastMove;
+
+    public ScrollerLayout(Context context) {
+        this(context, null);
+    }
+
+    public ScrollerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mScroller = new Scroller(context);
+        ViewConfiguration viewConfiguration = ViewConfiguration.get(context);
+        mTouchSlop = viewConfiguration.getScaledPagingTouchSlop();
+        Log.e("ScrollerLayout  ", "mTouchSlop=" + mTouchSlop);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View childView = getChildAt(i);
+            // 为ScrollerLayout中的每一个子控件测量大小
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+        if (b) {
+            int childCount = getChildCount();
+            for (int j = 0; j < childCount; j++) {
+                View view = getChildAt(j);
+                Log.e("view", "==" + view.getMeasuredWidth());
+                view.layout(0, j * view.getMeasuredHeight(), view.getMeasuredWidth(), (j + 1) * view.getMeasuredHeight());
+            }
+            Log.e("onLayout  ", "childCount=" + childCount);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mYDown = ev.getRawY();
+                mYLastMove = mYDown;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mYMove = ev.getRawY();
+                float diff = Math.abs(mYMove - mYDown);
+                mYLastMove = mYMove;
+                if (diff > mTouchSlop) {
+                    return true;
+                }
+                break;
+        }
+
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                mYMove = event.getRawY();
+                float scrollY = mYLastMove - mYMove;
+                scrollBy(0, (int) scrollY);
+                mYLastMove = mYMove;
+                break;
+            case MotionEvent.ACTION_UP:
+                // 当手指抬起时，根据当前的滚动值来判定应该滚动到哪个子控件的界面
+//                int targetIndex = (getScrollX() + getWidth() / 2) / getWidth();
+//                int dx = targetIndex * getWidth() - getScrollX();
+                // 第二步，调用startScroll()方法来初始化滚动数据并刷新界面
+                mScroller.startScroll(0, getScrollY(), 0, -getScrollY());
+                invalidate();
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+
+    @Override
+    public void computeScroll() {
+        // 第三步，重写computeScroll()方法，并在其内部完成平滑滚动的逻辑
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            invalidate();
+        }
+//        super.computeScroll();
+    }
+}
